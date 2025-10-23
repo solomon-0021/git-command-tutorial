2ffa1c8b63b7e01f3e2d92b9a080a89d4a7c2a58
Solomon Kahsay <solkahsay1994@gmail.com>
2025-10-23 07:55:11 +0300

    number guessing game

--- learngit.cpp
+++ learngit.cpp
@@ -14,16 +14,33 @@ int main (){
     cout << "Now we are in the reverse way" << endl;
     cout << "Using git push command" << endl; */
     
-    // a loop to print a retangulare pattern
+    // a loop to print a rectangulare pattern
 
     // outer loop to control the row
     for ( int i = 0; i < 4; i++){
         // inner loop to print the pattern
     for (int j = 0; j <= i; j++){
-        cout << "# " :
+        cout << "# " ;
     }
         cout << endl;
     }
+    
+    int guess, target = 15;
+    cout << "Enter your number to guess(1-50): ";
+    cin >> guess;
+    if (guess == target){
+        cout << "You guessed the target";
+    }
+    else if (guess < target){
+        cout << "You guess to low";
+    }
+    else{
+        cout << "You guess to high";
+    }
+
+
+
+cout << "Now I am working on my local editor(Vscode)" << endl;
 
     return 0;
 }
